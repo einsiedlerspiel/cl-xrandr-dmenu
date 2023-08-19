08341bac7b9a6024a52ff78f1cec4cb1a60afe2a
Lukas Woell <lukas.woell@posteo.de>
2023-08-19 18:35:49 +0200

    Update Copyright Note

--- xrandr.lisp
+++ xrandr.lisp
@@ -1,3 +1,18 @@
+;; Copyright (C) 2023  Lou Woell
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 (in-package :cl-xrandr-dmenu)
 
 (defparameter *xrandrcmd* "xrandr")
@@ -5,7 +20,7 @@
 (defparameter *launchercmd* "dmenu")
 (defparameter *launcherparam* (list "-i"
                                     "-b")
-  "options for `*launchercmd'")
+  "options for `*launchercmd*'")
 
 ;; #############
 ;; UI definition
